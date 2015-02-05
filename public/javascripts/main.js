df75660401880647dab5bc4103556a40d9aead97
Codective <codective@gmail.com>
2015-02-04 19:17:01 -0800

    Test Angular

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,8 @@
-console.log(this);
+var team = ["niolmccartney", "TLawesomeness", "Spinaldash", "EmileMuny", "tommyjanszen", "soyzamudio", "GrYoT", "cadenichols"];
+
+var app = angular.module("socialBoard", [])
+
+app.controller("MainController", ['$http', '$scope', function($http, $scope) {
+  $scope.ready = true;
+  $scope.tweeters = [];
+}]);
