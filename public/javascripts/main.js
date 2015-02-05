ea455abeabac88dc2aeb2f384a83344f9752545d
Codective <codective@gmail.com>
2015-02-04 21:06:28 -0800

    MVP

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,6 +3,14 @@ var team = ["niolmccartney", "TLawesomeness", "Spinaldash", "EmileMuny", "tommyj
 var app = angular.module("socialBoard", [])
 
 app.controller("MainController", ['$http', '$scope', function($http, $scope) {
-  $scope.ready = true;
-  $scope.tweeters = [];
+  $scope.tweeters = team.map(function(handle) { return { loading: true, screen_name: handle } });
+
+  $scope.tweeters.forEach(function(tweeter) {
+    $http.get("/tweeter/" + tweeter.screen_name).success(function(data) {
+      tweeter.retweets = data.retweets;
+      tweeter.favorites = data.favorites;
+      tweeter.sum = data.favorites + data.retweets;
+      tweeter.loading = false;
+    });
+  });
 }]);
