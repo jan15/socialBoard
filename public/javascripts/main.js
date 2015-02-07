03cd2f852e38341950bde99f819b973903f12ca6
Codective <codective@gmail.com>
2015-02-06 19:57:19 -0800

    MVP for localstorage

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
-var app = angular.module("socialBoard", [])
+var app = angular.module("socialBoard", []);
+
+var handles = JSON.parse(localStorage["tweeters"] || "[]");
 
 app.controller("MainController", ['$http', '$scope', function($http, $scope) {
   $scope.tweeters = [];
@@ -6,8 +8,13 @@ app.controller("MainController", ['$http', '$scope', function($http, $scope) {
   $scope.loading = false;
 
   $scope.addNewHandle = function() {
+
     $scope.loading = true;
 
+    if (handles.indexOf($scope.newHandle) < 0) {
+      handles.push($scope.newHandle);
+      localStorage["tweeters"] = JSON.stringify(handles);
+    }
     if ($scope.tweeters.map(function(tweeter) { return tweeter.screen_name }).indexOf($scope.newHandle) >= 0) {
       $scope.error = "Twitter handle already exists";
       return;
@@ -22,6 +29,7 @@ app.controller("MainController", ['$http', '$scope', function($http, $scope) {
       tweeter.favorites = data.favorites;
       tweeter.weight = data.favorites + (1.5 * data.retweets);
       $scope.error = false;
+      console.log($scope.newHandle);
       $scope.newHandle = '';
       $scope.tweeters.push(tweeter);
       $scope.loading = false;
@@ -31,4 +39,10 @@ app.controller("MainController", ['$http', '$scope', function($http, $scope) {
       $scope.newHandle = '';
     });
   };
+
+  handles.forEach(function(handle) {
+    $scope.newHandle = handle
+    $scope.addNewHandle();
+  });
+
 }]);
